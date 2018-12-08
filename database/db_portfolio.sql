a3adf1b7e2398e2e459258e7d3cd180e84f8356a
abhishek <patelabhi585@gmail.com>
2018-12-08 00:33:18 -0500

    added latest database

--- database/db_portfolio.sql
+++ database/db_portfolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 03, 2018 at 05:35 AM
+-- Generation Time: Dec 08, 2018 at 05:31 AM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
