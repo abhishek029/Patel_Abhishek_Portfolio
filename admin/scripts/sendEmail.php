de078861c0abb50da59d218a7f5d5ae188ee991c
abhishek <patelabhi585@gmail.com>
2018-12-09 16:26:22 -0500

    added admin folder for sending emails

---
+++ admin/scripts/sendEmail.php
@@ -0,0 +1,30 @@
+<?php
+ include ("/includes/connect.php");
+	function send_email(){
+		if(empty($_POST['email'])){
+			header('Location:/contact.php?status=error');
+			exit();
+		}
+
+		if(!isset($_POST['message']) || !isset($_POST['email'])){
+			header('Location:/contact.php?status=error');
+			exit();
+		}
+		
+		$name = $_POST['name'];
+		$to = 'patelabhi585@gmail.com';
+		$subject = "Portfolio Site Feedback";
+		$message = 'Message Body:'.$_POST['message'];
+		$headers = "From: noreply@ABHIPATEL.ca\r\n";
+		$headers .= 'Reply-To:'.$_POST['email'];
+		
+        
+		if(mail($to, $subject, $message, $headers)){
+			header('Location:/index.html');
+		}
+	}
+	
+	send_email();
+
+	
+?>
